b437f3cb23377d2b741d62843e18a40b52df7fe9
christine-aqui <aqui.christine@gmail.com>
2018-10-29 11:54:54 -0400

    adding comments to the index js to provide context when reviewing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// importing the required dependencies 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
@@ -8,15 +9,23 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+// Here you create the httpLink that will connect your ApolloClient instance 
+// with the GraphQL API, 
+// your GraphQL server will be running on http://localhost:4000.
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+// Now you instantiate ApolloClient by passing in the httpLink 
+// and a new instance of an InMemoryCache.
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 })
-
+ 
+// Finally you render the root component of your React app. 
+// The App is wrapped with the higher-order component ApolloProvider 
+// that gets passed the client as a prop.
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
